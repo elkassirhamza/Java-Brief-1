71ea456e977dc0f189a0cc31319d3e95233157ef
elkassirhamza <57805968+elkassirhamza@users.noreply.github.com>
2020-12-08 15:44:38 +0100

    Update functions

--- Boucles/src/Function.java
+++ Boucles/src/Function.java
@@ -1,17 +1,72 @@
 public class Function {
-    static boolean rtn(){
+    public static void main() {
+        // III- 1-
+
+    }
+
+    // III- 1-
+    public boolean ff() {
         return true;
     }
-    static String chara(String chaine){
-        System.out.println(chaine);
-        return chaine;
+
+    // III- 2-
+    public String str(String text) {
+        return text;
     }
 
-    public static void main(String[] args) {
-        //rtn();
-       // chara("Test Test");
+    // III- 3-
+    public String concat(String text1, String text2) {
+        return text1 + " " + text2;
     }
 
+    // III- 4-
+    public void numbers(Double nm1, Double nm2) {
+        if (nm1 > nm2) {
+            System.out.println(nm1 + ", Is too big");
+        } else if (nm1 < nm2) {
+            System.out.println(nm1 + ", Is too small");
+        } else {
+            System.out.println(nm1 + ", Is Equal to ," + nm2);
+        }
+    }
 
+    // III- -5
+    public void concatStrNum(String str, Double num) {
+        System.out.println(str.concat(String.valueOf(num)));
+    }
 
+    // III- -6
+    public void bonjour(String fName, String lName, Integer age) {
+        System.out.println("Hello ," + fName + lName + ",you have " + age + " years old");
+    }
+
+    // III- -7
+    public void sexGenre(String genre, Integer age) {
+        if (genre.equals("homme") || genre.equals("Homme") && age >= 18) {
+            System.out.println("Vous êtes un homme et vous êtes majeur");
+        } else if (genre.equals("homme") || genre.equals("Homme") && age < 18) {
+            System.out.println("Vous êtes un homme et vous êtes mineur");
+        } else if (genre.equals("femme") || genre.equals("Femme") && age >= 18) {
+            System.out.println("Vous êtes une femme et vous êtes majeur");
+        } else if (genre.equals("femme") || genre.equals("Femme") && age < 18) {
+            System.out.println("Vous êtes une femme et vous êtes mineur");
+        }
+    }
+
+    // III- -8
+    static int exercice8 (int Par1, int Par2, int Par3){
+        return (Par1 + Par2 + Par3);
+
+    }
+    static int exercice8 (int Par1, int Par2){
+        return exercice8( Par1, Par2, 5);
+    }
+    static int exercice8 (int Par1){
+        return exercice8( Par1, 3);
+    }
+    static int exercice8 (){
+        return exercice8( 2);
+    }
 }
+
+
